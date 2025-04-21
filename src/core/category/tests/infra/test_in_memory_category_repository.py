e729b1794721b5e1b18d88872b86628a161c2f93
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-04-21 15:10:19 -0300

    get by id in memory repository unit tests

--- src/core/category/tests/infra/test_in_memory_category_repository.py
+++ src/core/category/tests/infra/test_in_memory_category_repository.py
@@ -2,7 +2,7 @@ from src.core.category.domain.category import Category
 from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
 
-class TestInMemoryCategoryRepository:
+class TestSave:
   def test_can_save_category(self):
     repository = InMemoryCategoryRepository()
     category = Category(
@@ -13,3 +13,19 @@ class TestInMemoryCategoryRepository:
 
     assert len(repository.categories) == 1
     assert repository.categories[0] == category
+
+class TestGetById:
+  def test_get_category_by_id(self):
+    repository = InMemoryCategoryRepository()
+    category = Category(
+      name="Movie"
+    )
+
+    repository.save(category)
+
+    assert repository.get_by_id(category.id) == category
+
+  def test_get_category_by_id_not_found(self):
+    repository = InMemoryCategoryRepository()
+
+    assert repository.get_by_id(1) is None
