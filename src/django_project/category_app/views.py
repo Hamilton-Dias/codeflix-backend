cd8dfd11f1098ddb8a80fb5ae72651ed6f5c39f7
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-04-23 13:02:02 -0300

    First View

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+class CategoryViewSet(viewsets.ViewSet):
+  def list(self, request: Request) -> Response:
+    return Response(status=HTTP_200_OK, data=[
+      {
+        "id": "exemplo",
+        "name": "exemplo",
+        "description": "exemplo",
+        "is_active": True
+      },
+      {
+        "id": "exemplo2",
+        "name": "exemplo2",
+        "description": "exemplo2",
+        "is_active": True
+      }
+    ])
+
