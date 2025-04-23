974c136625e9fc3a0110bc9dc640b03d248c16d3
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-04-23 20:07:05 -0300

    Test API

--- src/django_project/category_app/tests.py
+++ src/django_project/category_app/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.status import HTTP_200_OK
 
-# Create your tests here.
+class TestCategoryAPI(APITestCase):
+  def test_list_categories(self):
+    url = '/api/categories/'
+    response = self.client.get(url)
+
+    expedted_data = [
+      {
+        "id": "exemplo",
+        "name": "exemplo",
+        "description": "exemplo",
+        "is_active": True
+      },
+      {
+        "id": "exemplo2",
+        "name": "exemplo2",
+        "description": "exemplo2",
+        "is_active": True
+      }
+    ]
+
+    self.assertEqual(response.status_code, HTTP_200_OK)
+    self.assertEqual(response.data, expedted_data)
