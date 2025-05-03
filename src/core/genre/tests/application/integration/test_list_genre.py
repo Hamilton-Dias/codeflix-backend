d56355c89d138f353fae2f677c56708b3549e535
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-05-03 09:54:38 -0300

    List Genre use case

---
+++ src/core/genre/tests/application/integration/test_list_genre.py
@@ -0,0 +1,50 @@
+from src.core.category.domain.category import Category
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+from src.core.genre.application.use_cases.list_genre import GenreOutput, ListGenre
+from src.core.genre.domain.genre import Genre
+from src.core.genre.infra.in_memory_genre_repository import InMemoryGenreRepository
+
+
+class TestListGenre:
+  def test_list_genres_with_associated_categories(self):
+    category_repository = InMemoryCategoryRepository()
+    genre_repository = InMemoryGenreRepository()
+    
+    movie_category = Category(name="Movie") 
+    documentary_category = Category(name="Documentary")
+
+    category_repository.save(movie_category)
+    category_repository.save(documentary_category)
+
+    genre = Genre(
+      name="Drama",
+      categories={movie_category.id, documentary_category.id}
+    )
+    genre_repository.save(genre)
+
+    use_case = ListGenre(repository=genre_repository)
+    output = use_case.execute(input=ListGenre.Input())
+
+    assert len(output.data) == 1
+    assert output == ListGenre.Output(
+      data=[
+        GenreOutput(
+          id=genre.id,
+          name=genre.name,
+          is_active=genre.is_active,
+          categories={
+            movie_category.id,
+            documentary_category.id
+          }
+        )
+      ]
+    )
+
+  def test_list_with_empty_genres(self):
+    genre_repository = InMemoryGenreRepository()
+    use_case = ListGenre(repository=genre_repository)
+    output = use_case.execute(input=ListGenre.Input())
+
+    assert len(output.data) == 0
+    assert output == ListGenre.Output(data=[])
+
---
+++ src/core/genre/tests/application/unit/test_list_genre.py
@@ -0,0 +1,53 @@
+from unittest.mock import create_autospec
+from src.core.genre.domain.genre import Genre
+from src.core.genre.domain.genre_repository import GenreRepository
+from src.core.genre.application.use_cases.list_genre import GenreOutput, ListGenre
+
+
+class TestListGenre:
+  def test_when_no_genres_in_repository_then_return_empty_list(self):
+    mock = create_autospec(GenreRepository)
+    mock.list.return_value = []
+
+    use_case = ListGenre(repository=mock)
+
+    output = use_case.execute(input=ListGenre.Input())
+    assert output == ListGenre.Output(data=[])
+
+  def test_when_genres_in_repository_then_return_list(self):
+    genres = [
+      Genre(
+        name='Genre 1',
+        categories=set()
+      ),
+      Genre(
+        name='Genre 2',
+        categories=set(),
+        is_active=False
+      )
+    ]
+
+    mock = create_autospec(GenreRepository)
+    mock.list.return_value = genres
+
+    use_case = ListGenre(repository=mock)
+
+    response = use_case.execute(input=ListGenre.Input())
+
+    assert response == ListGenre.Output(
+      data=[
+        GenreOutput(
+          id=genres[0].id,
+          name=genres[0].name,
+          is_active=genres[0].is_active,
+          categories=set()
+        ),
+        GenreOutput(
+          id=genres[1].id,
+          name=genres[1].name,
+          is_active=genres[1].is_active,
+          categories=set()
+        )
+      ]
+    )
+
