5b23a24ba1021c84ac026e43eea1b6b67e0c08e2
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-05-17 19:33:15 -0300

    Message bus and test

--- src/core/_shared/events/message_bus.py
+++ src/core/_shared/events/message_bus.py
@@ -1,7 +1,15 @@
+from typing import Type
 from src.core._shared.events.abstract_message_bus import AbstractMessageBus
 from src.core._shared.events.event import Event
+from src.core._shared.application.handler import Handler
 
 
-class MessageBus(AbstractMessageBus):
+class MessageBus(AbstractMessageBus):  
+  def __init__(self) -> None:
+    self.handlers: dict[Type[Event], list[Handler]] = {}
+
   def handle(self, events: list[Event]) -> None:
-    print("Handling events:", events)
+    for event in events:
+      handlers = self.handlers[type(event)]
+      for handler in handlers:
+        handler.handle(event)
---
+++ src/core/_shared/tests/events/test_message_bus.py
@@ -0,0 +1,18 @@
+from unittest.mock import create_autospec
+from src.core._shared.application.handler import Handler
+from src.core._shared.events.message_bus import MessageBus
+from src.core._shared.events.event import Event
+
+class DummyEvent(Event):
+  pass
+
+class TestMessageBus:
+  def test_calls_correct_handler_with_events(self):
+    message_bus = MessageBus()
+    dummy_handler = create_autospec(Handler)
+    message_bus.handlers[DummyEvent] = [dummy_handler]
+
+    event = DummyEvent()
+    message_bus.handle([event])
+
+    dummy_handler.handle.assert_called_once_with(event)
