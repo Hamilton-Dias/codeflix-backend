2a7aa15716b9f8a4ab19bcc749b632a11b81771e
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-05-19 21:05:05 -0300

    Keycloak and jwt auth service

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Application written in python 3 for Full Cycle 3.0 course.
 - Events Driven Archtecture
 - Unit and Integration tests
 - E2E Tests
+- Keycloak
 
 
 To run the program:
@@ -19,6 +20,7 @@ source .venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
 docker run -d --hostname rabbitmq --name rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker run -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:26.2.4 start-dev
 python manage.py startconsumer
 python manage.py runserver
 </pre>
---
+++ src/core/_shared/infrastructure/auth/jwt_auth_service.py
@@ -0,0 +1,22 @@
+import os
+
+import jwt
+from src.core._shared.infrastructure.auth.auth_service_interface import AuthServiceInterface
+
+
+class JwtAuthService(AuthServiceInterface):
+  def __init__(self, token: str = "") -> None:
+    self.public_key = os.getenv("AUTH_PUBLIC_KEY")
+    self.token = token.replace("Bearer ", "", 1)
+
+  def _decode_token(self) -> dict:
+    try:
+      return jwt.decode(self.token, self.public_key, algorithms=["RS256"], audience="account")
+    except jwt.PyJWTError:
+      return {}
+  
+  def is_authenticated(self) -> bool:
+    return bool(self._decode_token())
+
+  def has_role(self, role: str) -> bool:
+    return role in self._decode_token().get("realm_access", {}).get("roles", [])
