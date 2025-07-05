928d9d0d3f9a3a6e250bac413693ac22b7940828
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-07-05 15:34:19 -0300

    GrachQL router

--- src2/infra/api/http/main.py
+++ src2/infra/api/http/main.py
@@ -4,12 +4,14 @@ from src2.infra.api.http.cast_member_router import router as cast_member_router
 from src2.infra.api.http.category_router import router as category_router
 from src2.infra.api.http.genre_router import router as genre_router
 from src2.infra.api.http.video_router import router as video_router
+from src2.infra.api.graphql.schema_pydantic import graphql_app as graphql_router
 
 app = FastAPI()
 app.include_router(category_router, prefix="/categories")
 app.include_router(cast_member_router, prefix="/cast_members")
 app.include_router(genre_router, prefix="/genres")
 app.include_router(video_router, prefix="/videos")
+app.include_router(graphql_router, prefix="/graphql")
 
 
 @app.get("/healthcheck/")
